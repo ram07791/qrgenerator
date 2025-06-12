9c402b7f923a108b992e4f264ea111366829bdfd
datta ram <ramzelda861@gmail.com>
2025-06-12 16:16:06 +0530

    Initial commit

---
+++ qr_code_generator.py
@@ -0,0 +1,8 @@
+import qrcode
+data=input("enter the text or url: ").strip()
+filename= input('enter the filename: ').strip()
+qr = qrcode.QRCode(box_size=10, border=4 )
+qr.add_data(data)
+image= qr.make_image(fill_color= 'black', back_color = 'white')
+image.save(filename)
+print(f'QR code saved as {filename}')
